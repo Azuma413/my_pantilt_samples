38beb8c3d0ce1a8754e2945eab6c500467199bd5
Azuma413 <hirekatsu0523@gmail.com>
2023-03-01 16:10:00 +0900

    バグ修正

--- src/pantilt_service_test.py
+++ src/pantilt_service_test.py
@@ -1,31 +1,47 @@
 #!/usr/bin/env python
 import rospy
 from dynamixel_workbench_msgs.srv import DynamixelCommand
+from dynamixel_workbench_msgs.srv import DynamixelCommandRequest
 from geometry_msgs.msg import Twist
 
 #rosrun turtlesim turtle_teleop_key
+servo_data1 = DynamixelCommandRequest()
+servo_data2 = DynamixelCommandRequest()
+pan_angle = 2000
+tilt_angle = 2000
 
 def callback1(data):
-    servo_data1 = DynamixelCommand()
-    servo_data2 = DynamixelCommand()
+    global servo_data1
+    global servo_data2
+    rospy.loginfo("callback1 called")
+    global pan_angle
+    global tilt_angle
     servo_data1.command = ""
     servo_data2.command = ""
     servo_data1.id = 1
     servo_data2.id = 2
     servo_data1.addr_name = "Goal_Position"
     servo_data2.addr_name = "Goal_Position"
-    servo_data1.value = data.linear.x
-    servo_data2.value = data.angular.z
+    pan_angle -= int(data.linear.x*10)
+    tilt_angle += int(data.angular.z*10)
+    servo_data1.value = pan_angle
+    servo_data2.value = tilt_angle
     result1 = s_cliant(servo_data1)
     result2 = s_cliant(servo_data2)
     rospy.loginfo("servo1:{}, servo2:{}".format(result1, result2))
 
 if __name__ == "__main__":
+    rospy.loginfo("test")
     rospy.init_node('pantilt_service_test')
-    rospy.wait_for_service('pantilt_service_test')
+    rospy.wait_for_service('/dynamixel_workbench/dynamixel_command')
     s_cliant = rospy.ServiceProxy('/dynamixel_workbench/dynamixel_command', DynamixelCommand)
-    sub = rospy.Subscriber('turtlebot_telop_keyboard/cmd_vel', Twist, callback1, queue_size=10)
+    sub = rospy.Subscriber('/turtle1/cmd_vel', Twist, callback1, queue_size=10)
     try:
+        #while not rospy.is_shutdown():
+        #    result1 = s_cliant(servo_data1)
+        #    result2 = s_cliant(servo_data2)
+        #    rospy.loginfo("servo1:{}, servo2:{}".format(result1, result2))
+        #    rospy.Rate(50).sleep()
         rospy.spin()
     except KeyboardInterrupt:
         pass
