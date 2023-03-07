46d5153ab3af0d2f37aa365e4d81cb7e36bf8f38
Azuma413 <hirekatsu0523@gmail.com>
2023-03-07 14:18:36 +0900

    a

--- src/pantilt_service_test.py
+++ src/pantilt_service_test.py
@@ -9,26 +9,24 @@ servo_data1 = DynamixelCommandRequest()
 servo_data2 = DynamixelCommandRequest()
 pan_angle = 2000
 tilt_angle = 2000
+servo_data1.command = ""
+servo_data2.command = ""
+servo_data1.id = 1
+servo_data2.id = 2
+servo_data1.addr_name = "Goal_Position"
+servo_data2.addr_name = "Goal_Position"
+servo_data1.value = pan_angle
+servo_data2.value = tilt_angle
 
 def callback1(data):
     global servo_data1
     global servo_data2
-    rospy.loginfo("callback1 called")
     global pan_angle
     global tilt_angle
-    servo_data1.command = ""
-    servo_data2.command = ""
-    servo_data1.id = 1
-    servo_data2.id = 2
-    servo_data1.addr_name = "Goal_Position"
-    servo_data2.addr_name = "Goal_Position"
     pan_angle -= int(data.linear.x*10)
     tilt_angle += int(data.angular.z*10)
     servo_data1.value = pan_angle
     servo_data2.value = tilt_angle
-    result1 = s_cliant(servo_data1)
-    result2 = s_cliant(servo_data2)
-    rospy.loginfo("servo1:{}, servo2:{}".format(result1, result2))
 
 if __name__ == "__main__":
     rospy.loginfo("test")
@@ -37,11 +35,10 @@ if __name__ == "__main__":
     s_cliant = rospy.ServiceProxy('/dynamixel_workbench/dynamixel_command', DynamixelCommand)
     sub = rospy.Subscriber('/turtle1/cmd_vel', Twist, callback1, queue_size=10)
     try:
-        #while not rospy.is_shutdown():
-        #    result1 = s_cliant(servo_data1)
-        #    result2 = s_cliant(servo_data2)
-        #    rospy.loginfo("servo1:{}, servo2:{}".format(result1, result2))
-        #    rospy.Rate(50).sleep()
-        rospy.spin()
+        while not rospy.is_shutdown():
+            result1 = s_cliant(servo_data1)
+            result2 = s_cliant(servo_data2)
+            rospy.loginfo("servo1:{}, servo2:{}".format(result1, result2))
+            rospy.Rate(50).sleep()
     except KeyboardInterrupt:
         pass
